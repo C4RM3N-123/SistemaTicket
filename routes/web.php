c56943bcd16df5682c84fa173ca7accd3ec86b10
Henry <Ghreengate@gmail.com>
2023-07-27 10:34:22 -0500

    Modificacion de la base de datos

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,12 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\CityController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'actionIndex']);
 
-Route::resource('admin', AdminController::class);
-Route::resource('employee', EmployeeController::class);
-Route::resource('user', UserController::class);
-Route::resource('ticket', TicketController::class);
-Route::resource('detailTicket', DetailTicketController::class);
-Route::resource('picture', PictureController::class);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('city/getall', [CityController::class, 'actionGetAll']);
+Route::match(['get', 'post'], 'city/insert', [CityController::class, 'actionInsert']);
+Route::get('city/delete/{idCity}', [CityController::class, 'actionDelete']);
+Route::match(['get', 'post'], 'city/update/{idCity}', [CityController::class, 'actionUpdate']);
